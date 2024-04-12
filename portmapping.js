caa45ee9211e4659d6757703104b24be9c6e0ab1
Samer Kouki <samer.kouki@bitctrl.de>
2024-04-12 08:18:14 +0200

    correct exports

--- portmapping.js
+++ portmapping.js
@@ -1,29 +1,17 @@
 "use strict";
 
-module.exports.portmapping = function(parent) {
+module.exports.portmapping = function (parent) {
   var obj = {};
   obj.parent = parent;
   obj.meshServer = parent.parent;
   obj.debug = obj.meshServer.debug;
-  obj.exports = [
-    "onWebUIStartupEnd",
-    "openSettings",
-    "goPageStart",
-    "mapUpdate",
-    "resizeContent",
-    "onDeviceRefreshEnd",
-    "setUserRdpLinks",
-    "updateUserRdpLinks",
-    "dlRDPfile",
-    "updateRdpDeviceLinks",
-    "cantMap"
-  ];
+  obj.exports = [];
 
-  obj.server_startup = function() {
+  obj.server_startup = function () {
     console.log("SERVER_STARTED");
   };
 
-  obj.hook_agentCoreIsStable = function(myparent, gp) {
+  obj.hook_agentCoreIsStable = function (myparent, gp) {
     console.log("AGENT_IS_CONNECTED");
   };
 
